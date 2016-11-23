5bdad5d4ffac716b7c0bdef360e3797dac484cff
Fashion+ <fashion@FashiondeMacBook-Pro.local>
2016-11-23 17:05:55 +0800

    Initial Commit

---
+++ InAppPurchaseTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InAppPurchaseTest
+//
+//  Created by Fashion+ on 2016/11/23.
+//  Copyright © 2016年 Fashion+. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
